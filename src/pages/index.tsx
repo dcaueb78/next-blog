4a90c19b6bc4d5b209e1c5d27a92d544e56885ff
Cauê Kotarski <caue1017@hotmail.com>
2025-06-04 14:07:31 -0300

    feat: create page header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,10 +16,7 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <h2>Hello world</h2>
-      <Button variant="secondary">
-        Button
-      </Button>
+      <Header />
     </div>
   );
 }
